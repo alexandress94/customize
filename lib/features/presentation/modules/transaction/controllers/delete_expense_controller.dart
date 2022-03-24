55da0277746bc6a8df751d89fa213f4df65627e5
alexandress94 <alexandre.sobral94s@gmail.com>
2022-03-24 09:48:25 -0300

    🔨 add: create enum and create function for delete with switch case

--- lib/features/presentation/modules/transaction/controllers/delete_expense_controller.dart
+++ lib/features/presentation/modules/transaction/controllers/delete_expense_controller.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
+import 'package:organize_more/features/domain/entities/expense_entity.dart';
 import 'package:organize_more/features/domain/usecases/delete_expense_usecase.dart';
 import 'package:organize_more/features/presentation/utils/show_snackbar.dart';
 
 import '../../../../../core/services/log/log.dart';
+
+enum Delete {
+  delete_one,
+  delete_between,
+  delete_all,
+}
 
 class DeleteExpenseController extends GetxController {
   final DeleteExpenseUsecase _usecase;
@@ -13,6 +20,25 @@ class DeleteExpenseController extends GetxController {
     required Log log,
   })  : _usecase = usecase,
         _log = log;
+
+  Rx<Delete> remove = Delete.delete_one.obs;
+
+  Future<void> deleteExpense(ExpenseEntity expense) async {
+    switch (remove.value) {
+      case Delete.delete_one:
+        await delete(expense.id!);
+        remove.value = Delete.delete_one;
+        break;
+      case Delete.delete_between:
+        print('delete between');
+        remove.value = Delete.delete_one;
+        break;
+      case Delete.delete_all:
+        print('Delete all');
+        remove.value = Delete.delete_one;
+        break;
+    }
+  }
 
   Future<void> delete(int id) async {
     final result = await _usecase.call(ParameterDeleteExpense(id: id));
