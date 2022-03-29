7b9ba074462f9622c0adfb12ae27e1417ab5b7b1
alexandress94 <alexandre.sobral94s@gmail.com>
2022-03-29 11:24:02 -0300

    ✨ feature: feat implementation delete all usecase

---
+++ lib/features/domain/usecases/delete_all_expense_usecase.dart
@@ -0,0 +1,37 @@
+import 'package:organize_more/core/usecases/usecase.dart';
+import 'package:organize_more/core/errors/usecase_exception.dart';
+import 'package:either_dart/either.dart';
+
+import '../repositories/delete_all_expense_repository.dart';
+
+class DeleteAllExpenseUsecase
+    implements Usecase<int, ParameterDeleteAllExpense> {
+  final DeleteAllExpenseRepository _repository;
+
+  DeleteAllExpenseUsecase({
+    required DeleteAllExpenseRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<Either<Exception, int>> call(
+    ParameterDeleteAllExpense parameter,
+  ) async {
+    if (parameter.uuId.isEmpty) {
+      return Left(UsecaseException.invalidInput());
+    }
+
+    final result = await _repository.deleteAll(uuId: parameter.uuId);
+
+    if (result.isLeft) {
+      return Left(result.left);
+    }
+
+    return Right(result.right);
+  }
+}
+
+class ParameterDeleteAllExpense {
+  final String uuId;
+
+  ParameterDeleteAllExpense({required this.uuId});
+}
