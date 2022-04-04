6b770d0f84fbe415723bfe4eae831759f5eb7591
alexandress94 <alexandre.sobral94s@gmail.com>
2022-04-04 15:55:48 -0300

    🚚 move: save feat pie chart

---
+++ lib/features/presentation/modules/fl_chart/widgets/sum_of_expenses_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../core/values/format/format_money.dart';
+import '../../transactions/controllers/get_all_expense_controller.dart';
+
+class SumOfExpensesWidget extends GetView<GetAllExpenseController> {
+  const SumOfExpensesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Total do mês',
+          style: Get.textTheme.bodyText1?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          width: 90,
+          child: FittedBox(
+            child: Obx(
+              () => Text(
+                FormatMoney.outputMask(
+                  controller.getSumOfTransactions.toString(),
+                ),
+                style: Get.textTheme.bodyText1,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
