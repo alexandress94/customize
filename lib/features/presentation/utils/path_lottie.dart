de5c88b1d69d9c68c2baf802756abeeac63851b9
alexandress94 <alexandre.sobral94s@gmail.com>
2022-04-04 22:06:11 -0300

    💄 ui: add new lottie space error

--- lib/features/presentation/utils/path_lottie.dart
+++ lib/features/presentation/utils/path_lottie.dart
@@ -4,5 +4,6 @@ abstract class PathLottie {
   static const REPAYMENT = 'assets/lottie/repayment.json';
   static const SAVING_MONEY = 'assets/lottie/saving-money.json';
   static const ERROR_404 = 'assets/lottie/error-404.json';
+  static const SPACE_ERROR_404 = 'assets/lottie/404-space-error.json';
   static const BUSINESS_ANALYTICS = 'assets/lottie/business-analytics.json';
 }
