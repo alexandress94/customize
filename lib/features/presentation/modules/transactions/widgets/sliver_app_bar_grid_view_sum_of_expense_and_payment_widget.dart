f7e45bd0cc8f40b265988ea9ddd2ebe6353cad92
alexandress94 <alexandre.sobral94s@gmail.com>
2022-04-07 15:39:40 -0300

    💄 ui: grud vuew over flow fix

--- lib/features/presentation/modules/transactions/widgets/sliver_app_bar_grid_view_sum_of_expense_and_payment_widget.dart
+++ lib/features/presentation/modules/transactions/widgets/sliver_app_bar_grid_view_sum_of_expense_and_payment_widget.dart
@@ -22,112 +22,227 @@ class SliverAppBarGridViewSumOfExpenseAndPaymentWidget
       centerTitle: true,
       pinned: false,
       flexibleSpace: FlexibleSpaceBar(
-        background: GridView.count(
-          physics: const NeverScrollableScrollPhysics(),
-          primary: false,
-          padding: const EdgeInsets.all(16.0),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Container(
-                padding: const EdgeInsets.all(24.0),
-                decoration: BoxDecoration(
-                  color: AppLightColors.appWhiteColor,
+        background: Padding(
+          padding: EdgeInsets.all(16.0.h),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: ClipRRect(
                   borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: AppLightColors.appBarBackgroundColor,
-                      ),
-                      child: const Icon(Icons.trending_up),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: AppLightColors.appWhiteColor,
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    SizedBox(height: 15.0.h),
-                    Text(
-                      'Despesa com pagamento',
-                      textAlign: TextAlign.left,
-                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                            letterSpacing: 1.5,
-                            color: AppLightColors.appSecondaryColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: AppLightColors.appBarBackgroundColor,
                           ),
-                    ),
-                    SizedBox(height: 15.0.h),
-                    Obx(
-                      () => FittedBox(
-                        child: Text(
-                          FormatMoney.outputMask(
-                            controller.getPaymentSum.toString(),
-                          ),
+                          child: const Icon(Icons.trending_up),
+                        ),
+                        SizedBox(height: 15.0.h),
+                        Text(
+                          'Despesa com pagamento',
+                          textAlign: TextAlign.left,
                           style:
                               Theme.of(context).textTheme.bodyText1?.copyWith(
+                                    letterSpacing: 1.5,
+                                    color: AppLightColors.appSecondaryColor,
+                                  ),
+                        ),
+                        SizedBox(height: 15.0.h),
+                        Obx(
+                          () => FittedBox(
+                            child: Text(
+                              FormatMoney.outputMask(
+                                controller.getPaymentSum.toString(),
+                              ),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.copyWith(
                                     fontSize: 18.0.h,
                                     fontWeight: FontWeight.bold,
                                   ),
+                            ),
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Container(
-                padding: const EdgeInsets.all(24.0),
-                decoration: BoxDecoration(
-                  color: AppLightColors.appWhiteColor,
+              SizedBox(width: 20.0.h),
+              Expanded(
+                child: ClipRRect(
                   borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: AppLightColors.appBarBackgroundColor,
-                      ),
-                      child: const Icon(Icons.trending_down),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: AppLightColors.appWhiteColor,
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    SizedBox(height: 15.0.h),
-                    Text(
-                      'Despesa sem pagamento',
-                      textAlign: TextAlign.left,
-                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                            letterSpacing: 1.5,
-                            color: AppLightColors.appSecondaryColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: AppLightColors.appBarBackgroundColor,
                           ),
-                    ),
-                    SizedBox(height: 15.0.h),
-                    Obx(
-                      () => FittedBox(
-                        child: Text(
-                          FormatMoney.outputMask(
-                            controller.getSumOfExpenses.toString(),
-                          ),
+                          child: const Icon(Icons.trending_down),
+                        ),
+                        SizedBox(height: 15.0.h),
+                        Text(
+                          'Despesa sem pagamento',
+                          textAlign: TextAlign.left,
                           style:
                               Theme.of(context).textTheme.bodyText1?.copyWith(
+                                    letterSpacing: 1.5,
+                                    color: AppLightColors.appSecondaryColor,
+                                  ),
+                        ),
+                        SizedBox(height: 15.0.h),
+                        Obx(
+                          () => FittedBox(
+                            child: Text(
+                              FormatMoney.outputMask(
+                                controller.getSumOfExpenses.toString(),
+                              ),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.copyWith(
                                     fontSize: 18.0.h,
                                     fontWeight: FontWeight.bold,
                                   ),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
+        // background: GridView.count(
+        //   physics: const NeverScrollableScrollPhysics(),
+        //   primary: false,
+        //   padding: const EdgeInsets.all(16.0),
+        //   crossAxisSpacing: 10,
+        //   mainAxisSpacing: 10,
+        //   crossAxisCount: 2,
+        //   children: [
+        //     ClipRRect(
+        //       borderRadius: BorderRadius.circular(20.0),
+        //       child: Container(
+        //         padding: const EdgeInsets.all(24.0),
+        //         decoration: BoxDecoration(
+        //           color: AppLightColors.appWhiteColor,
+        //           borderRadius: BorderRadius.circular(20.0),
+        //         ),
+        //         child: Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Container(
+        //               height: 40,
+        //               width: 40,
+        //               decoration: BoxDecoration(
+        //                 borderRadius: BorderRadius.circular(10),
+        //                 color: AppLightColors.appBarBackgroundColor,
+        //               ),
+        //               child: const Icon(Icons.trending_up),
+        //             ),
+        //             SizedBox(height: 15.0.h),
+        //             Text(
+        //               'Despesa com pagamento',
+        //               textAlign: TextAlign.left,
+        //               style: Theme.of(context).textTheme.bodyText1?.copyWith(
+        //                     letterSpacing: 1.5,
+        //                     color: AppLightColors.appSecondaryColor,
+        //                   ),
+        //             ),
+        //             SizedBox(height: 15.0.h),
+        //             Obx(
+        //               () => FittedBox(
+        //                 child: Text(
+        //                   FormatMoney.outputMask(
+        //                     controller.getPaymentSum.toString(),
+        //                   ),
+        //                   style:
+        //                       Theme.of(context).textTheme.bodyText1?.copyWith(
+        //                             fontSize: 18.0.h,
+        //                             fontWeight: FontWeight.bold,
+        //                           ),
+        //                 ),
+        //               ),
+        //             )
+        //           ],
+        //         ),
+        //       ),
+        //     ),
+        //     ClipRRect(
+        //       borderRadius: BorderRadius.circular(20.0),
+        //       child: Container(
+        //         padding: const EdgeInsets.all(24.0),
+        //         decoration: BoxDecoration(
+        //           color: AppLightColors.appWhiteColor,
+        //           borderRadius: BorderRadius.circular(20.0),
+        //         ),
+        //         child: Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Container(
+        //               height: 40,
+        //               width: 40,
+        //               decoration: BoxDecoration(
+        //                 borderRadius: BorderRadius.circular(10),
+        //                 color: AppLightColors.appBarBackgroundColor,
+        //               ),
+        //               child: const Icon(Icons.trending_down),
+        //             ),
+        //             SizedBox(height: 15.0.h),
+        //             Text(
+        //               'Despesa sem pagamento',
+        //               textAlign: TextAlign.left,
+        //               style: Theme.of(context).textTheme.bodyText1?.copyWith(
+        //                     letterSpacing: 1.5,
+        //                     color: AppLightColors.appSecondaryColor,
+        //                   ),
+        //             ),
+        //             SizedBox(height: 15.0.h),
+        //             Obx(
+        //               () => FittedBox(
+        //                 child: Text(
+        //                   FormatMoney.outputMask(
+        //                     controller.getSumOfExpenses.toString(),
+        //                   ),
+        //                   style:
+        //                       Theme.of(context).textTheme.bodyText1?.copyWith(
+        //                             fontSize: 18.0.h,
+        //                             fontWeight: FontWeight.bold,
+        //                           ),
+        //                 ),
+        //               ),
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     )
+        //   ],
+        // ),
         centerTitle: true,
       ),
     );
