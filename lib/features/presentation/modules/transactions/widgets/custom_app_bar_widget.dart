0e0e9cd45104eaafa23aea8db1c681632f529dc2
alexandress94 <alexandre.sobral94s@gmail.com>
2022-04-08 23:01:49 -0300

    💄 ui: add new design app bar

--- lib/features/presentation/modules/transactions/widgets/custom_app_bar_widget.dart
+++ lib/features/presentation/modules/transactions/widgets/custom_app_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:organize_more/features/presentation/theme/app_color.dart';
 
 class CustomAppBarWidget extends StatelessWidget {
   const CustomAppBarWidget({Key? key}) : super(key: key);
@@ -7,11 +8,22 @@ class CustomAppBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 0.0,
-      title: Text(
-        'Minhas despesas',
-        style: Theme.of(context).textTheme.headline5?.copyWith(
-              fontWeight: FontWeight.bold,
-            ),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Organize mais',
+            style: Theme.of(context).textTheme.headline5?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          Text(
+            'minhas despesas',
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: AppLightColors.appSecondaryColor,
+                ),
+          ),
+        ],
       ),
       actions: [
         IconButton(
