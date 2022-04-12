a3dddc69a6b8e456fdaeb082f9bcb8322f1603a9
alexandress94 <alexandre.sobral94s@gmail.com>
2022-04-11 23:10:51 -0300

    🐛 bug: bug not save theme dark

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:organize_more/core/services/bindings/initial_bindings.dart';
 import 'package:organize_more/features/presentation/routes/routes.dart';
+import 'core/services/service_controller/theme_service_controller.dart';
 import 'features/presentation/routes/app_pages.dart';
 import 'features/presentation/theme/app_theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -31,8 +32,7 @@ void main() async {
         initialBinding: InitialBindings(),
         theme: AppTheme().appLightThemeData,
         darkTheme: AppTheme().appDarkThemeData,
-        // themeMode: ThemeServiceController().getThemeMode(),
-        themeMode: ThemeMode.system,
+        themeMode: ThemeServiceController().getThemeMode(),
       ),
     ),
   );
