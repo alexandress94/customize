033befb10eb644be361ca97dafe3313c22be35d6
alexandress94 <alexandre.sobral94s@gmail.com>
2022-05-14 17:16:40 -0300

    💄 ui: implementation toggle switch

---
+++ lib/features/presentation/modules/expense/widgets/toggle_switch_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:organize_more/features/presentation/modules/expense/controllers/insert_or_update_expense_controller.dart';
+import 'package:organize_more/features/presentation/theme/app_color.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+class ToggleSwitchWidget extends GetView<InsertOrUpdateExpenseController> {
+  final String labelOne;
+  final String labelTwo;
+  final void Function(int?)? onToggle;
+
+  const ToggleSwitchWidget({
+    Key? key,
+    required this.labelOne,
+    required this.labelTwo,
+    required this.onToggle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: ToggleSwitch(
+        minWidth: double.infinity.w,
+        minHeight: 50.0.h,
+        initialLabelIndex: 0,
+        cornerRadius: 20.0,
+        activeFgColor: Colors.white,
+        inactiveBgColor: Get.isDarkMode
+            ? AppDarkColors.appSecondaryBackgroundColor
+            : AppLightColors.appWhiteColor,
+        inactiveFgColor: Get.isDarkMode
+            ? AppDarkColors.appWhiteColor
+            : AppLightColors.appSecondaryColor,
+        totalSwitches: 2,
+        labels: [labelOne, labelTwo],
+        activeBgColors: Get.isDarkMode
+            ? [
+                [AppDarkColors.appBlueColor, AppDarkColors.appBlueColor],
+                [AppDarkColors.appBlueColor, AppDarkColors.appBlueColor]
+              ]
+            : [
+                [AppLightColors.appBlackColor, AppLightColors.appBlackColor],
+                [AppLightColors.appBlackColor, AppLightColors.appBlackColor]
+              ],
+        animate: true,
+        curve: Curves.easeInOutCubicEmphasized,
+        onToggle: onToggle,
+      ),
+    );
+  }
+}
