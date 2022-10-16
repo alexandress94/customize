974a9efc30d6ec2e2734d16476f0a29988d3c1f0
alexandress94 <alexandre.sobral94s@gmail.com>
2022-10-16 00:35:12 -0300

    ✏️ fix: inject bind

--- lib/features/presentation/modules/splash/binding/splash_binding.dart
+++ lib/features/presentation/modules/splash/binding/splash_binding.dart
@@ -1,9 +1,18 @@
+import 'package:customize/core/services/local_notification/local_notification_service.dart';
+import 'package:customize/core/services/packages/contracts/local_get_storage.dart';
+import 'package:customize/core/services/service_controller/check_native_darkmode_service_controller.dart';
 import 'package:get/get.dart';
 import 'package:customize/features/presentation/modules/splash/controller/splash_controller.dart';
 
 class SplashBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put(SplashController(storage: Get.find(), service: Get.find()));
+    Get.put(
+      SplashController(
+        storage: Get.find<LocalGetStorage>(),
+        service: Get.find<CheckNativeDarkmodeServiceController>(),
+        notificationService: Get.find<LocalNotificationService>(),
+      ),
+    );
   }
 }
