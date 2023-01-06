c7c2db9fe160f5b3b1d51a47cd11b00cdaf98209
alexandress94 <alexandre.sobral94s@gmail.com>
2023-01-06 16:42:41 -0300

    mock: create variable for mock of list expense

---
+++ test/core/mock/list_expense_model.dart
@@ -0,0 +1,17 @@
+var tListExpenseModel = [
+  {
+    'id': 1,
+    'uuId': '5792f0fc-8a27-4cc0-8848-12f6089f1b2f',
+    'ds_transaction': 'conta de agua',
+    'vl_transaction': 150.0,
+    'vl_total': 150.0,
+    'nr_installment': 1,
+    'qt_installment': 1,
+    'yn_payment': 0,
+    'yn_portion': 0,
+    'tp_transaction': 'Mensal',
+    'dt_payment': "2012-02-27",
+    'dt_transaction': "2012-02-27",
+    'dt_due': "2012-02-27",
+  }
+];
