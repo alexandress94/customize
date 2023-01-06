baa4a13a5f94430a2fcd819faab1e30c6fa52c97
alexandress94 <alexandre.sobral94s@gmail.com>
2023-01-06 15:18:08 -0300

    test: testing of insert datas

---
+++ test/features/domain/usecases/insert_expense_usecase_test.dart
@@ -0,0 +1,59 @@
+import 'package:customize/core/errors/usecase_exception.dart';
+import 'package:customize/features/domain/repositories/insert_expense_repository.dart';
+import 'package:customize/features/domain/usecases/insert_expense_usecase.dart';
+import 'package:either_dart/either.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../core/mock/expense_id.dart';
+import '../../../core/mock/insert_expense.dart';
+
+class InsertExpenseRepositoryMock extends Mock
+    implements InsertExpenseRepository {}
+
+void main() {
+  late InsertExpenseUsecase sut;
+  late InsertExpenseRepository repository;
+
+  setUp(() {
+    repository = InsertExpenseRepositoryMock();
+    sut = InsertExpenseUsecase(repository: repository);
+  });
+
+  group('Test get all expense usecase:', () {
+    test('Should be return success when the result for a right', () async {
+      when(() => repository.insertExpense(expense: tInsertExpense)).thenAnswer(
+        (_) async => Right<Exception, int>(tExpenseId),
+      );
+
+      final result = await sut.call(
+        ParameterInsertExpense(expenses: tInsertExpense),
+      );
+
+      expect(result.isRight, true);
+    });
+    test('Should be return success when the result for a id', () async {
+      when(() => repository.insertExpense(expense: tInsertExpense)).thenAnswer(
+        (_) async => Right<Exception, int>(tExpenseId),
+      );
+
+      final result = await sut(
+        ParameterInsertExpense(expenses: tInsertExpense),
+      );
+
+      expect(result.right, tExpenseId);
+    });
+
+    test('Should be return failure when the result for a left', () async {
+      when(() => repository.insertExpense(expense: tInsertExpense)).thenAnswer(
+        (_) async => Left<Exception, int>(UsecaseException.invalidInput()),
+      );
+
+      final result = await sut(
+        ParameterInsertExpense(expenses: tInsertExpense),
+      );
+
+      expect(result.isLeft, true);
+    });
+  });
+}
