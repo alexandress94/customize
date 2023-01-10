e22bb7ff7821580a37540be15d1ec870b751821e
alexandress94 <alexandre.sobral94s@gmail.com>
2023-01-10 11:09:48 -0300

    refactor: refactoring implementation of package url launcher for depreciated

--- lib/core/services/packages/gateways/url_launcher_implementation.dart
+++ lib/core/services/packages/gateways/url_launcher_implementation.dart
@@ -3,14 +3,18 @@ import 'package:url_launcher/url_launcher.dart';
 import '../contracts/url_launcher.dart';
 
 class UrlLaucherImplementation implements UrlLaucher {
-  String _url =
-      'mailto:alexandre.sobral94s@gmail.com?subject=Suporte Organize despesa&body=';
-
   @override
   Future<void> open() async {
-    if (!await launch(_url,
-        forceSafariVC: false, forceWebView: false, enableJavaScript: true)) {
-      throw 'Could not launch $_url';
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: 'help.customize@gmail.com',
+      query: 'subject=Suporte Customize&body=', //add subject and body here
+    );
+
+    if (!await canLaunchUrl(params)) {
+      await launchUrl(params);
+    } else {
+      throw 'Could not launch $params';
     }
   }
 }
